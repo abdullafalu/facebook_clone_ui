199c9c1fbc99ff893c1e92305bf911f107cf10cb
abdullafalu <abdullafalu55@gmail.com>
2022-12-22 12:10:30 +0530

    show banner true

--- facebook_clone/lib/home.dart
+++ facebook_clone/lib/home.dart
@@ -14,7 +14,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
